aa9d309fb2296e211bdcc8636754db60d1c9ae81
Martin Di Paola <martinp.dipaola@gmail.com>
2017-07-29 23:16:08 -0300

    Run meld for test comparision
    The test runner script can spawn a meld (graphical diff) to compare the
    output of a test and its expected output.

--- regress/test.sh
+++ regress/test.sh
@@ -5,8 +5,29 @@ fail() {
     exit 1
 }
 
-[ $# -eq 1 ] || fail "Usage: $0 <test-name>"
-[ -x "./regress/$1.sh" ]   || fail "Test $1 not found."
-[ -f "./regress/$1.good" ] || fail "Expected 'good' output for test $1 not found."
+if [ $# -eq 2 ] 
+then
+    action="$1"
+    t="$2"
+elif [ $# -eq 1 ]
+then
+    action="run"
+    t="$1"
+else
+    fail "Usage: $0 <test-name>"
+fi
 
-sudo "./regress/$1.sh" > "regress/$1.found" 2>&1 ; diff "regress/$1.found" "regress/$1.good"
+[ "$action" == "run" -o "$action" == "meld" ] || fail "Action $action did not understand."
+
+[ -x "./regress/$t.sh" ]   || fail "Test $t not found."
+[ -f "./regress/$t.good" ] || fail "Expected 'good' output for test $t not found."
+
+if [ "$action" == "run" ]
+then
+    sudo "./regress/$t.sh" > "regress/$t.found" 2>&1 ; diff "regress/$t.found" "regress/$t.good"
+elif [ "$action" == "meld" ]
+then
+    meld "regress/$t.found" "regress/$t.good"
+else
+    fail "Action $action did not understand."
+fi
